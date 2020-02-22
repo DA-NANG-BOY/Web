de6cd3bb9123c2fd6123b170d8d02f17c02a2016
Bob15324 <ryze152x@gmail.com>
2020-02-22 11:27:24 +0700

    change

--- BackEnd/Main.js
+++ BackEnd/Main.js
@@ -1,3 +1,19 @@
+import database from "./database";
+import sanpham from "./sanpham";
+import đăng from "./sanpham";
+
+
+let sanpham = new sanpham();
+let database = new database();
+let đăng = new đăng();
+
+function check_sanpham(name,soluong,nguoidung) {
+
+}
+
+function sanpham_hot(name,soluong,master) {
+
+}
 const loading = document.getElementById('loading');
 function xóa() {
     document.body.removeChild(loading);
--- BackEnd/sanpham.js
+++ BackEnd/sanpham.js
@@ -1,9 +1,37 @@
 import database from "../FrontEnd/database.js"
 
-export default function sanpham(){
-     this.name = [];
+let data = new database();
+
+export default function sanpham(name, soluong,master) {
+      this.name_where = document.getElementById('');
+      this.name_where = [...name_where];
+      this.soluong_where = document.getElementById('');
+      this.master_where = document.getElementById('');
+      check();
+}
+
+function check(name) {
+       
+}
+
+export default function đăng(){
+     this.src = null ;
+     this.img = null ;
+     this.master = null;
+     this.name = null ;
      this.soluong = null ;
      this.init = function () {
-          
+          (async() => {
+            this.src = await document.getElementById('picture-link').value ;
+            this.
+            
+            this.master = await document.getElementById('chủ-sản-phẩm');
+           this.name = await document.getElementById('tên-sản-phẩm');
+           this.soluong = await document.getElementById('so-luong-san-pham');
+           sanpham(this.name, this.soluong , this.master);
+          })();
      }  
 };
+function sanpham_hot(name,soluong) {
+       
+}
