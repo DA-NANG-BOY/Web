2f5599c16f902ee0d3189a4266172ecaa06c414a
Bob15324 <ryze152x@gmail.com>
2020-03-28 22:03:45 +0700

    change

--- BackEnd/Main.js
+++ BackEnd/Main.js
@@ -1,25 +1,3 @@
-import database from "./database";
-import sanpham from "./sanpham";
-import đăng from "./sanpham";
-
-
-
-
-function check(name,soluong,master) {
-  this.name = name ;
-  this.soluong = soluong;
-  this.master = master ;
-  this.Checked() = function () {
-
-  } 
-}
-
-function sanpham_hot(name,soluong,master) {
-      
-    this.loop = function (){
-    setTimeout(loop(),3000)
-}
-}
 const loading = document.getElementById('loading');
 function xóa() {
     document.body.removeChild(loading);
